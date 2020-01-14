fc2f41913f644951a78a2c6d765c85d9679c4971
Nathan Keller <keller.nathan@gmail.com>
2020-01-14 09:45:25 -0700

    Iteration 2 comeplete. I know there is a better way to do "vendors_that_sell" but shipping code at the moment

---
+++ lib/market.rb
@@ -0,0 +1,26 @@
+class Market
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor.inventory.keys.find do |single_item|
+        single_item == item
+      end
+    end
+  end
+end
